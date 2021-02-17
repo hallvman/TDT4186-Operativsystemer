f874ae1a3a5bef5c8cefca7ae3b3241b586bac4b
hallvman <hallvman@stud.ntnu.no>
2021-02-17 11:02:14 +0100

    Oppgave 3

--- PE2/catchTheZombies.c
+++ PE2/catchTheZombies.c
@@ -1,20 +1,30 @@
-#include <unistd.h> 
-#include <stdio.h> 
+#include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
  
 int main(){
-	int t, pid;
+	int t, pid, wp;
 
 	while(1){
         printf("\nEnter number: ");
         scanf("%d", &t);
 
+        system("ps -e | grep catchTheZombies");
+
+        do {
+            wp = waitpid(-1, NULL, WNOHANG);
+        } while (wp > 0);
+
         pid = fork();
         if(pid > 0){
+            // Creates a new child of the number you entered
             printf("You created a new child, id: %d\n", pid);
 		}
         if (pid == 0){
             sleep(t);
+            printf("\nDeceased child: %d\n", getpid());
             printf("\nRING! Time: %d Child ID: %d\n", t, getpid());
             exit(0);
         }
