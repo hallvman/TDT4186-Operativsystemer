6256a0201a07cf2761320b90c50493f9d7aa9179
Hallvard <Hallv-a@otmail.bo>
2021-03-16 16:42:50 +0100

    Fått gjor read og split, men ikke execute

--- PE4/shell.c
+++ PE4/shell.c
@@ -4,9 +4,10 @@
 #include <stdio.h>
 #include <string.h>
 
-void loop(void);
+#define TOKEN_BUFSIZE 64
+#define TOKEN_DELIM " \t\r\n\a"
 
-void readLine(void){
+char *readLine(void){
 
     char *line = NULL;
     // Buffer
@@ -23,25 +24,76 @@ void readLine(void){
   return line;
 }
 
+char **splitLine(char *line){
+    int bufsize = TOKEN_BUFSIZE, position = 0;
+    char **tokens = malloc(bufsize * sizeof(char*));
+    char *token;
+
+    if (!tokens)
+    {
+        printf("Error");
+        exit(EXIT_FAILURE);
+    }
+    
+    token = strtok(line, TOKEN_DELIM);
+    while (token != NULL)
+    {
+        tokens[position] = token;
+        position++;
+
+        if (position >= bufsize)
+        {
+            bufsize += TOKEN_BUFSIZE;
+            tokens = realloc(tokens, bufsize * sizeof(char*));
+
+            if (!tokens)
+            {
+                printf("Error");
+                exit(EXIT_FAILURE);
+            }
+
+        }
+        token = strtok(NULL, TOKEN_DELIM);  
+        
+
+    }
+    
+    tokens[position] = NULL;
+    return tokens;
+}
+
+int executeLine(char **tokens){
+
+    pid_t pid;
+    int status;
+
+    if (tokens[0] == NULL)
+    {
+        printf("ERROR: Invalid commando");
+        return 1;
+    }
+
+    
+}
+
 int main(int argc, char **argv){
 
     // The terminal loop
-    loop();
-
-    return 0;
-}
-
-void loop(void){
-
     char *line;
+    char **tokens;
+    int status;
 
     do
     {
-
         printf("€ ");
         line = readLine();
+        tokens = splitLine(line);
+        status = executeLine(tokens);
         
-    } while (1);
+        // Terminates for memory
+        free(line);
+        free(tokens);
+    } while (status);
 
+    return EXIT_SUCCESS;
 }
-
