643c31131845618b867bd260c29ec9cf5b66f22e
Borgar Føll Flytør <borgarff@stud.ntnu.no>
2021-04-11 20:11:51 +0200

    Added pipe example from linux man paged

--- PE5/pipeline.c
+++ PE5/pipeline.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
 #include <unistd.h>
  
  //PE5 
@@ -12,11 +13,54 @@ int main(void) {
 
   int child_pipe[2];
   int parent_pipe[2];
+  pid_t pid;
 
-  if (pipe(child_pipe) != 0 || pipe(parent_pipe) != 0) {
-    perror("Faild to create pipe one or more pipe");
+  if (pipe(child_pipe) == -1) {
+    perror("Faild to create child pipe");
   }
-  
+
+  if (pipe(parent_pipe) == -1) {
+    perror("Faild to create parent pipe");
+  }
+
 
   return EXIT_SUCCESS;
 }
+
+//Pipe example from the pipe() man page in Linux
+/*
+#include <stdlib.h>
+#include <unistd.h>
+...
+
+int fildes[2];
+const int BSIZE = 100;
+char buf[BSIZE];
+ssize_t nbytes;
+int status;
+
+status = pipe(fildes);
+if (status == -1 ) {
+    // an error occurred 
+    ...
+}
+
+switch (fork()) {
+case -1: // Handle error 
+    break;
+
+case 0:  // Child - reads from pipe 
+    close(fildes[1]);                       // Write end is unused 
+    nbytes = read(fildes[0], buf, BSIZE);   // Get data from pipe 
+    // At this point, a further read would see end-of-file ... 
+    close(fildes[0]);                       // Finished with pipe 
+    exit(EXIT_SUCCESS);
+
+default:  // Parent - writes to pipe 
+    close(fildes[0]);                       // Read end is unused 
+    write(fildes[1], "Hello world\n", 12);  // Write data on pipe 
+    close(fildes[1]);                       // Child will see EOF 
+    exit(EXIT_SUCCESS);
+}
+
+*/
